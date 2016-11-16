9c18e2e9c5ccfcd9f037f45f94a83038dc16a71a
Matteo Baglini <matteo.baglini@gmail.com>
2016-11-16 11:07:42 +0100

    Remove obsolete test

--- src/TriviaRefactoringKata/QuestionDeckTests.cs
+++ src/TriviaRefactoringKata/QuestionDeckTests.cs
@@ -6,29 +6,6 @@ namespace Trivia
 {
     public class QuestionDeckTests
     {
-        [Theory]
-        [InlineData(0, "Pop")]
-        [InlineData(4, "Pop")]
-        [InlineData(8, "Pop")]
-        [InlineData(1, "Science")]
-        [InlineData(5, "Science")]
-        [InlineData(9, "Science")]
-        [InlineData(2, "Sports")]
-        [InlineData(6, "Sports")]
-        [InlineData(10, "Sports")]
-        [InlineData(3, "Rock")]
-        [InlineData(7, "Rock")]
-        [InlineData(11, "Rock")]
-        public void CategoryForBoardPlace_(Int32 place, String expected)
-        {
-            var deck = new QuestionDeck();
-
-            deck.FillQuestions();
-            var category = deck.CategoryForPlace(place);
-
-            Assert.Equal(expected, category);
-        }
-
         [Fact]
         public void CategoryForPlace()
         {
