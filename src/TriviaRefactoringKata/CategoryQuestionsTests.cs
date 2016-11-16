9f30f56351e05a80ba29d9cf75f86d20f7ee8c9b
Matteo Baglini <matteo.baglini@gmail.com>
2016-11-16 09:43:19 +0100

    Add test lists for CategoryQeustions

---
+++ src/TriviaRefactoringKata/CategoryQuestionsTests.cs
@@ -0,0 +1,13 @@
+﻿namespace Trivia
+{
+    public class CategoryQuestionsTests
+    {
+        // check in places
+        // check out of places
+        // first questions
+        // many questions
+        // out of questions
+
+
+    }
+}
