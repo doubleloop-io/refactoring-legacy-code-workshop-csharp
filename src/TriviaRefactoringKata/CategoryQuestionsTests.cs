ee51d728e5620b5a46d8874becbbb9d829daa2a2
Matteo Baglini <matteo.baglini@gmail.com>
2016-11-16 09:46:26 +0100

    Implementes test con category questions place

--- src/TriviaRefactoringKata/CategoryQuestionsTests.cs
+++ src/TriviaRefactoringKata/CategoryQuestionsTests.cs
@@ -1,13 +1,26 @@
-﻿namespace Trivia
+﻿using Xunit;
+
+namespace Trivia
 {
     public class CategoryQuestionsTests
     {
-        // check in places
-        // check out of places
         // first questions
         // many questions
         // out of questions
 
+        [Fact]
+        public void PlacedOnAskedPosition()
+        {
+            var categoryQuestions = new CategoryQuestions("not useful", new[] {5, 9});
+            Assert.True(categoryQuestions.Contains(5));
+            Assert.True(categoryQuestions.Contains(9));
+        }
 
+        [Fact]
+        public void NotPlacedOnAskedPosition()
+        {
+            var categoryQuestions = new CategoryQuestions("not useful", new[] {5, 9});
+            Assert.False(categoryQuestions.Contains(6));
+        }
     }
 }
