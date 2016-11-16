10c7e5d36fd378093f36327cc6abc2a3fb73ae9e
Matteo Baglini <matteo.baglini@gmail.com>
2016-11-16 09:34:24 +0100

    Add CategoryQuestions implementation

--- src/TriviaRefactoringKata/CategoryQuestions.cs
+++ src/TriviaRefactoringKata/CategoryQuestions.cs
@@ -1,28 +1,37 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace Trivia
 {
     public class CategoryQuestions
     {
+        readonly Int32[] places;
+        readonly LinkedList<String> questions;
         public String Name { get; }
 
         public CategoryQuestions(String name, Int32[] places)
         {
+            questions = new LinkedList<String>();
+            this.places = places;
             Name = name;
         }
 
         public void AddQuestion(String question)
         {
+            questions.AddLast(question);
         }
 
         public Boolean Contains(Int32 place)
         {
-            return false;
+            return places.Contains(place);
         }
 
         public String NextQuestion()
         {
-            return null;
+            var result = questions.First();
+            questions.RemoveFirst();
+            return result;
         }
     }
 }
